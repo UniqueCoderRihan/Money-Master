930df03522a44277e6d5bf4be1d771ce3a96b10b
Rihanul Islam <rihanthedev@gmail.com>
2023-02-16 17:32:17 +0600

    try to do useing function

--- scripts/functional.js
+++ scripts/functional.js
@@ -1,3 +1,4 @@
+
 function calculete(elementId){
     const ELements = document.getElementById(elementId);
     const FiledString = ELements.value;
@@ -14,7 +15,13 @@ function totalCost(){
     return total;
 }
 
+function setText(elementId, NewValue){
+    const mainFiled = document.getElementById(elementId);
+    const valueSet = mainFiled.innerText = NewValue;
+}
+
 
 document.getElementById('Calculete-btn').addEventListener('click',function(){
-   
+   const totalCostFiled = totalCost();
+   const valueSetForTotal =setText('totalExpenses-filed',totalCostFiled);
 })
---
+++ scripts/noteForTask.d
@@ -0,0 +1,3 @@
+1. frist function for the Every Filed who Give Return Me Amount.
+2. secound Function for the total AMount calculete.
+3
